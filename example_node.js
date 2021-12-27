cd1c608b49b6ab9911df8f5a00f3cb45878b645d
Solitone <31255212+jgei@users.noreply.github.com>
2021-12-27 12:49:44 +0100

    Changed payload

--- example_node.js
+++ example_node.js
@@ -3,7 +3,7 @@ module.exports = function(RED) {
         RED.nodes.createNode(this,config);
         var node = this;
         node.on('input', function(msg) {
-            msg.payload = "TAIJI HAGINO";
+            msg.payload = "Judith Geimer";
             node.send(msg);
         });
     }
